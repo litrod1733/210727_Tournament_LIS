827ca235b641af9c86f3a4b976d3446842bdb56d
이인성 <isaac@iinseongs-MacBook-Pro.local>
2021-07-25 17:37:41 +0900

    solution

---
+++ cmake-build-debug/CMakeFiles/210727_Tournament_LIS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "210727_Tournament_LIS"
+  "210727_Tournament_LIS.pdb"
+  "CMakeFiles/210727_Tournament_LIS.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/210727_Tournament_LIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/210727_Tournament_LIS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 210727_Tournament_LIS.
+# This may be replaced when dependencies are built.
